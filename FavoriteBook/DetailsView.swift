669f263a6b4c1cab53f24a2312120f23be14f002
Onurcan Kurt <onurcankurt96@gmail.com>
2023-11-03 03:07:17 +0300

    DetailsView codes added

--- FavoriteBook/DetailsView.swift
+++ FavoriteBook/DetailsView.swift
@@ -8,11 +8,24 @@
 import SwiftUI
 
 struct DetailsView: View {
+    
+    var chosenFavoriteElement : FavoriteElements
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Image(chosenFavoriteElement.imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            Text(chosenFavoriteElement.name)
+                .font(.largeTitle)
+                .padding()
+            
+            Text(chosenFavoriteElement.description)
+        }
+        
     }
 }
 
 #Preview {
-    DetailsView()
+    DetailsView(chosenFavoriteElement: thedarknight)
 }
